9cfdc76838fabc5f1dbe87581437d911cba3d46c
Oleg Kapustin <oleg@blindmate.de>
2025-10-29 15:26:57 +0100

    backend: generate basic teachers and students REST resources.

--- sirius-backend/src/app.module.ts
+++ sirius-backend/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
+import { TeachersModule } from './teachers/teachers.module';
+import { StudentsModule } from './students/students.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    TeachersModule,
+    StudentsModule,
+  ],
   controllers: [],
   providers: [],
 })
